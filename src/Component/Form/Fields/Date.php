9b6ed469bdedad41439ca57970542c300885eaae
tangtanglove <dai_hang_love@126.com>
2021-10-11 14:55:04 +0800

    fix Date Component

--- src/Component/Form/Fields/Date.php
+++ src/Component/Form/Fields/Date.php
@@ -12,4 +12,39 @@ class Date extends Item
      * @var string
      */
     public $component = 'dateField';
+
+    /**
+     * 设置选择器类型,date | week | month | quarter | year
+     *
+     * @var string
+     */
+    public $picker = 'date';
+
+    /**
+     * 设置选择器类型,date | week | month | quarter | year
+     *
+     * @param  string $picker
+     * @return $this
+     */
+    public function picker($picker)
+    {
+        if(!in_array($picker,['date', 'week', 'month', 'quarter', 'year'])) {
+            throw new Exception("argument must be in 'date', 'week', 'month', 'quarter', 'year'!");
+        }
+
+        $this->picker = $picker;
+        return $this;
+    }
+
+    /**
+     * 组件json序列化
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return array_merge([
+            'picker' => $this->picker
+        ], parent::jsonSerialize());
+    }
 }
