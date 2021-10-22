4c9ecc58b853eaa9e4998c55c0dee951cb1b03e0
tangtanglove <dai_hang_love@126.com>
2021-10-22 13:29:07 +0800

    fix Tabs

--- src/Component/Tabs/Tabs.php
+++ src/Component/Tabs/Tabs.php
@@ -81,6 +81,15 @@ class Tabs extends Element
     }
 
     /**
+     * Pane控件
+     *
+     * @var array
+     */
+    public static $classFields = [
+        'pane' => TabPane::class
+    ];
+
+    /**
      * 标签居中展示
      *
      * @param  bool  $centered
@@ -195,6 +204,43 @@ class Tabs extends Element
         $this->tabPanes = $tabPanes;
 
         return $this;
+    }
+
+    /**
+     * 获取行为类
+     *
+     * @param string $method
+     * @return bool|mixed
+     */
+    public static function getCalledClass($method)
+    {
+        $class = static::$classFields[$method];
+
+        if (class_exists($class)) {
+            return $class;
+        }
+
+        return false;
+    }
+
+    /**
+     * 动态调用行为类
+     *
+     * @param string $method
+     * @return bool|mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if ($className = static::getCalledClass($method)) {
+
+            $column = $parameters[0]; // 列字段
+            $label = $parameters[1] ?? null; // 标题
+            $callback = $parameters[2] ?? null; // 回调函数
+
+            $element = new $className($column, $label, $callback);
+
+            return $element;
+        }
     }
 
     /**
