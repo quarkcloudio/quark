5efd3204f294661d4fc22e352e597be998b38a59
tangtanglove <dai_hang_love@126.com>
2021-11-11 17:17:27 +0800

    optimize the PageContainer component

--- src/Component/Layout/PageContainer.php
+++ src/Component/Layout/PageContainer.php
@@ -8,81 +8,11 @@ use Closure;
 class PageContainer extends Element
 {
     /**
-     * 自定义标题文字
-     *
-     * @var string
-     */
-    public $title;
-
-    /**
-     * 自定义的二级标题文字
-     *
-     * @var string
-     */
-    public $subTitle;
-
-    /**
-     * pageHeader 的类型，将会改变背景颜色
-     *
-     * @var bool
-     */
-    public $ghost = true;
-
-    /**
-     * 标题栏旁的头像
-     *
-     * @var string
-     */
-    public $avatar;
-
-    /**
-     * 自定义 back icon ，如果为 false 不渲染 back icon，默认：<ArrowLeft />
-     *
-     * @var string
-     */
-    public $backIcon;
-
-    /**
-     * title 旁的 tag 列表
-     *
-     * @var array
-     */
-    public $tags;
-
-    /**
-     * 操作区，位于 title 行的行尾
-     *
-     * @var array
-     */
-    public $extra;
-
-    /**
-     * 面包屑的配置
-     *
-     * @var array
-     */
-    public $breadcrumb;
-
-    /**
-     * PageHeader 的页脚，一般用于渲染 TabBar
-     *
-     * @var array
-     */
-    public $footer;
-
-    /**
-     * 返回按钮
-     *
-     * @var bool|string
-     */
-    public $backButton = false;
-
-    /**
      * 内容区
      *
      * @var array
      */
-    public $body;
+    public $content;
 
     /**
      * 额外内容区，位于 content 的右侧
@@ -92,6 +22,13 @@ class PageContainer extends Element
     public $extraContent;
 
     /**
+     * tab 标题列表
+     *
+     * @var array
+     */
+    public $tabList;
+
+    /**
      * 当前高亮的 tab 项
      *
      * @var string
@@ -99,11 +36,76 @@ class PageContainer extends Element
     public $tabActiveKey;
 
     /**
-     * tab bar 上额外的元素
+     * tab bar 上额外的元素	
      *
      * @var array
      */
     public $tabBarExtraContent;
+
+    /**
+     * PageHeader 的所有属性
+     *
+     * @var array
+     */
+    public $header;
+
+    /**
+     * 配置头部区域的背景颜色为透明
+     *
+     * @var bool
+     */
+    public $ghost = false;
+
+    /**
+     * 固定 pageHeader 的内容到顶部，如果页面内容较少，最好不要使用，会有严重的遮挡问题
+     *
+     * @var bool
+     */
+    public $fixedHeader;
+
+    /**
+     * 固钉的配置，与 antd 完全相同
+     *
+     * @var array
+     */
+    public $affixProps;
+
+    /**
+     * 悬浮在底部的操作栏，传入一个数组，会自动加空格
+     *
+     * @var array
+     */
+    public $footer;
+
+    /**
+     * 内容区
+     *
+     * @var array
+     */
+    public $body;
+
+    /**
+     * 配置水印，Layout 会透传给 PageContainer，但是以 PageContainer 的配置优先
+     *
+     * @var array
+     */
+    public $waterMarkProps;
+
+    /**
+     * Tabs 的相关属性，只有卡片样式的页签支持新增和关闭选项。使用 closable={false} 禁止关闭
+     *
+     * @var array
+     */
+    public $tabProps;
+
+    /**
+     * 动态类
+     *
+     * @var array
+     */
+    public static $classes = [
+        'pageHeader' => PageContainer\PageHeader::class,
+    ];
 
     /**
      * 初始化容器
@@ -112,63 +114,141 @@ class PageContainer extends Element
      * @param  \Closure|array  $body
      * @return void
      */
-    public function __construct($title = '', $body = [])
+    public function __construct($header = '', $body = [])
     {
         $this->component = 'pageContainer';
-        $this->title = $title;
+        $this->header = $header;
         $this->body = $body;
 
         return $this;
     }
 
     /**
-     * 设置标题文字
+     * 内容区
      *
-     * @param  string  $title
+     * @param  string|array  $content
      * @return $this
      */
-    public function title($title)
+    public function content($content)
     {
-        $this->title = $title;
+        $this->content = $content;
 
         return $this;
     }
 
     /**
-     * 设置二级标题文字
+     * 额外内容区，位于 content 的右侧
      *
-     * @param  string  $subTitle
+     * @param  array  $extraContent
      * @return $this
      */
-    public function subTitle($subTitle)
+    public function extraContent($extraContent)
     {
-        $this->subTitle = $subTitle;
+        $this->extraContent = $extraContent;
 
         return $this;
     }
 
     /**
-     * 面包屑
+     * tab 标题列表
      *
-     * @param  array  $breadcrumb
+     * @param  array  $tabList
      * @return $this
      */
-    public function breadcrumb($breadcrumb)
+    public function tabList($tabList)
     {
-        $this->breadcrumb = $breadcrumb;
+        $this->tabList = $tabList;
 
         return $this;
     }
 
     /**
-     * 返回按钮
+     * 当前高亮的 tab 项
      *
-     * @param  bool|string  $backButton
+     * @param  string  $tabActiveKey
      * @return $this
      */
-    public function backButton($backButton = true)
+    public function tabActiveKey($tabActiveKey)
     {
-        $this->backButton = $backButton;
+        $this->tabActiveKey = $tabActiveKey;
+
+        return $this;
+    }
+
+    /**
+     * tab bar 上额外的元素
+     *
+     * @param  string|array  $tabBarExtraContent
+     * @return $this
+     */
+    public function tabBarExtraContent($tabBarExtraContent)
+    {
+        $this->tabBarExtraContent = $tabBarExtraContent;
+
+        return $this;
+    }
+
+    /**
+     * PageHeader 的所有属性
+     *
+     * @param  array  $header
+     * @return $this
+     */
+    public function header($header)
+    {
+        $this->header = $header;
+
+        return $this;
+    }
+
+    /**
+     * 配置头部区域的背景颜色为透明
+     *
+     * @param  bool  $ghost
+     * @return $this
+     */
+    public function ghost($ghost = true)
+    {
+        $this->ghost = $ghost;
+
+        return $this;
+    }
+
+    /**
+     * 固定 pageHeader 的内容到顶部，如果页面内容较少，最好不要使用，会有严重的遮挡问题
+     *
+     * @param  bool  $fixedHeader
+     * @return $this
+     */
+    public function fixedHeader($fixedHeader = true)
+    {
+        $this->fixedHeader = $fixedHeader;
+
+        return $this;
+    }
+
+    /**
+     * 固钉的配置，与 antd 完全相同
+     *
+     * @param  array  $affixProps
+     * @return $this
+     */
+    public function affixProps($affixProps)
+    {
+        $this->affixProps = $affixProps;
+
+        return $this;
+    }
+
+    /**
+     * 悬浮在底部的操作栏，传入一个数组，会自动加空格
+     *
+     * @param  array  $footer
+     * @return $this
+     */
+    public function footer($footer)
+    {
+        $this->footer = $footer;
 
         return $this;
     }
@@ -187,6 +267,66 @@ class PageContainer extends Element
     }
 
     /**
+     * 配置水印，Layout 会透传给 PageContainer，但是以 PageContainer 的配置优先
+     *
+     * @param  array  $waterMarkProps
+     * @return $this
+     */
+    public function waterMarkProps($waterMarkProps)
+    {
+        $this->waterMarkProps = $waterMarkProps;
+
+        return $this;
+    }
+
+    /**
+     * Tabs 的相关属性，只有卡片样式的页签支持新增和关闭选项。使用 closable={false} 禁止关闭
+     *
+     * @param  array  $tabProps
+     * @return $this
+     */
+    public function tabProps($tabProps)
+    {
+        $this->tabProps = $tabProps;
+
+        return $this;
+    }
+
+    /**
+     * 获取行为类
+     *
+     * @param string $method
+     * @return bool|mixed
+     */
+    public static function getCalledClass($method)
+    {
+        return $class = static::$classes[$method];
+
+        if (class_exists($class)) {
+            return $class;
+        }
+
+        return false;
+    }
+
+    /**
+     * 动态调用行为类
+     *
+     * @param string $method
+     * @return bool|mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if ($className = static::getCalledClass($method)) {
+            $title = $parameters[0]; // 标题
+            $subTitle = $parameters[1] ?? null; // 子标题
+            $element = new $className($title, $subTitle);
+
+            return $element;
+        }
+    }
+
+    /**
      * 组件json序列化
      *
      * @return array
@@ -194,15 +334,23 @@ class PageContainer extends Element
     public function jsonSerialize()
     {
         if(empty($this->key)) {
-            $this->key(__CLASS__.$this->title.$this->subTitle, true);
+            $this->key(json_encode($this->header).json_encode($this->tabList), true);
         }
 
         return array_merge([
-            'title' => $this->title,
-            'subTitle' => $this->subTitle,
-            'breadcrumb' => $this->breadcrumb,
-            'backButton' => $this->backButton,
-            'body' => $this->body
+            'content' => $this->content,
+            'extraContent' => $this->extraContent,
+            'tabList' => $this->tabList,
+            'tabActiveKey' => $this->tabActiveKey,
+            'tabBarExtraContent' => $this->tabBarExtraContent,
+            'header' => $this->header,
+            'ghost' => $this->ghost,
+            'fixedHeader' => $this->fixedHeader,
+            'affixProps' => $this->affixProps,
+            'footer' => $this->footer,
+            'body' => $this->body,
+            'waterMarkProps' => $this->waterMarkProps,
+            'tabProps' => $this->tabProps
         ], parent::jsonSerialize());
     }
 }
